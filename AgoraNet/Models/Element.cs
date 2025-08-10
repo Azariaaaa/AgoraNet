f14a6f1d7b2698a45c488c8a2e2d9eb9ce983e51
Louis Kuster <macbeth574@gmail.com>
2025-08-11 00:28:10 +0200

    Created Element model + properties

---
+++ AgoraNet/Models/Element.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgoraNet.Core.Models
+{
+    public class Element
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public string FillColor { get; set; } = "#808080";
+        public double Opacity { get; set; }
+    }
+}
