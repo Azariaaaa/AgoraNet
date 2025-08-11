9fb6f10a5c4a2acf5febeebc15d75fa8259bbe12
Louis Kuster <macbeth574@gmail.com>
2025-08-11 14:05:53 +0200

    Filed Text model class with properties and methods

--- AgoraNet/Models/Text.cs
+++ AgoraNet/Models/Text.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace AgoraNet.Core.Models
 {
-    public class Text
+    public class Text : Element
     {
+        public string Content { get; set; } = "";
+        public string FontFamily { get; set; } = "Arial";
+        public double FontSize { get; set; } = 16;
+
+        public Text(double x, double y, double z, string content, string fillColor)
+        : base(x, y, z)
+        {
+            Content = content;
+            FillColor = fillColor;
+        }
+
+        public override string ToSvg()
+        {
+            return $"<text x=\"{X}\" y=\"{Y}\" z=\"{Z}\" fill=\"{FillColor}\" font-family=\"{FontFamily}\" font-size=\"{FontSize}\">{System.Security.SecurityElement.Escape(Content)}</text>";
+        }
     }
 }
