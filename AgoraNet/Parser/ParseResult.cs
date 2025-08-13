da403495ea4a64466da1b3a5d24bdfdef5046f47
Louis Kuster <macbeth574@gmail.com>
2025-08-13 18:42:35 +0200

    Added properties to PageResult model class

--- AgoraNet/Parser/ParseResult.cs
+++ AgoraNet/Parser/ParseResult.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AgoraNet.Core.Models;
 
 namespace AgoraNet.Core.Parser
 {
     public class ParseResult
     {
-        //A coder
+        public bool Success { get; set; }
+        public List<Element> Elements { get; set; } = new List<Element>();
     }
 }
