1f37d3583b51d2a19a07ecd92e573a7097569162
Louis Kuster <macbeth574@gmail.com>
2025-08-15 14:19:33 +0200

    Created ParseFile, GetRawElements, ParseList and ParseRawElement methods in VdlParser

--- AgoraNet/Parser/VdlParser.cs
+++ AgoraNet/Parser/VdlParser.cs
@@ -2,20 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AgoraNet.Core.Models;
 
 namespace AgoraNet.Core.Parser
 {
-    public class VdlParser
+    public static class VdlParser
     {
         public static ParseResult ParseFile(string path)
         {
-            string FileContent = File.ReadAllText(path);
-            // A coder 
-            // A coder 
-            // A coder 
+            string fileContent = File.ReadAllText(path);
+            List<string> rawElements = GetRawElements(fileContent);
             return new ParseResult();
+        }
+
+        private static List<string> GetRawElements(string input)
+        {
+            MatchCollection matches = Regex.Matches(input, @"\[(.*?)\]");
+            List<string> rawElements = matches.Cast<Match>().Select(m => m.Groups[1].Value).ToList();
+            return rawElements;
+        }
+
+        private static List<Element> ParseList(List<string> rawElements)
+        {
+            List<Element> elements = new List<Element>();
+            foreach (string rawElement in rawElements)
+            {
+                Element element = ParseRawElement(rawElement);
+                elements.Add(element);
+            }
+            return elements;
+        }
+
+        private static Element ParseRawElement(string rawElement)
+        {
+            throw new NotImplementedException();
         }
     }
 }
