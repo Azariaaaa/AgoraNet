a59cf659dc3e95a8b02a32026aaf8e5ac80a1846
Louis Kuster <macbeth574@gmail.com>
2025-08-20 12:39:05 +0200

    Moved IElementFactory in Parser -> Core and added ParserConfig + ElementsRegistery class

--- AgoraNet/Parser/Core/ElementsRegistery.cs
+++ AgoraNet/Parser/Core/ElementsRegistery.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AgoraNet.Core.Parser.Factories
+namespace AgoraNet.Core.Parser.Core
 {
-    public interface IElementFactory
+    public class ElementsRegistery
     {
     }
 }
---
+++ AgoraNet/Parser/Core/ParserConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgoraNet.Core.Parser.Core
+{
+    public class ParserConfig
+    {
+    }
+}
