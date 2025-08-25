bb1c0ac50643d4d8ccb41da7cc1a3beb07cab4e5
Louis Kuster <macbeth574@gmail.com>
2025-08-25 10:48:44 +0200

    Added logic in ParserConfig

--- AgoraNet.Core/Parser/Core/ParserConfig.cs
+++ AgoraNet.Core/Parser/Core/ParserConfig.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AgoraNet.Core.Parser.Factories;
 
 namespace AgoraNet.Core.Parser.Core
 {
-    public class ParserConfig
+    public static class ParserConfig
     {
+        private static bool _initialized = false;
+
+        public static void RegisterDefaults()
+        {
+            if (_initialized) 
+                return;
+
+            ElementsRegistry.Register("Square", new SquareFactory());
+            ElementsRegistry.Register("Text", new TextFactory());
+
+            _initialized = true;
+        }
     }
 }
